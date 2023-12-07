6212a339efb5cded8b3af4cf8620b4f95773f15a
Brendon Lima <31414691+bredbk@users.noreply.github.com>
2023-12-07 20:05:46 -0300

    Making it work

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,71 @@
 import { Request, Response, Router } from "express";
+import { Readable } from "stream";
+import readline from "readline";
 
 import multer from "multer";
+import { prismaClient } from "./database/client";
 
 const multerConfig = multer();
 
 const router = Router();
 
-router.post("/products", multerConfig.single("file"), (request: Request, response: Response) => {
-    console.log(request.file?.buffer.toString("utf-8"));
+interface IClient {
+    id: number
+    nome: string
+    altura: number
+    lactose: boolean
+    peso: number
+    atleta: boolean
+}
 
+router.post("/clients", multerConfig.single("file"), async (request: Request, response: Response) => {
+    const { file } = request;
+    if (file !== undefined) {
 
-    return response.send();
+        const { buffer } = file;
+        const readableFile = new Readable();
+        readableFile.push(buffer);
+        readableFile.push(null);
+
+        const clientLine = readline.createInterface({
+            input: readableFile
+        });
+
+        const clients: IClient[] = [];
+
+        for await (let line of clientLine) {
+            const clientLineSplit = line.split(";");
+
+            
+            clients.push({
+                id: Number(clientLineSplit[0]),
+                nome: clientLineSplit[1].trim(),
+                altura: Number(parseFloat(clientLineSplit[2].replace(',', '.'))),
+                lactose: clientLineSplit[3] === "0" ? false : true,
+                peso: parseFloat(clientLineSplit[4].replace(',', '.')),
+                atleta: clientLineSplit[5] === "0" ? false : true
+            });
+            
+            
+        } 
+        for await (let { id, nome, altura, lactose, peso, atleta } of clients) {
+            await prismaClient.clients.create({
+                data: {
+                    id,
+                    nome,
+                    altura,
+                    lactose,
+                    peso,
+                    atleta
+                }
+            });
+            console.log("Client created successfully");
+        }
+        return response.json(clients);
+    } else {
+        console.log("File is undefined");
+    }
+
 });
 
-router.get("/ola", (request: Request, response: Response) => {
-    return response.json({ message: "Olá" });
-});
 export { router };
